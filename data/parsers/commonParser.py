497a5df8205d42df3b76df014b9ea7db74033dd1
elilcatness <elilcatness@gmail.com>
2021-06-24 23:35:23 +0300

    first commit (default parser, dixy parser, a little bit of structure)

---
+++ data/parsers/commonParser.py
@@ -0,0 +1,51 @@
+import logging
+from selenium.webdriver import Chrome, ChromeOptions
+from threading import Timer
+from exceptions import InheritanceError
+
+
+class CommonParser:
+    url: str = None
+    interval: int = None
+
+    def __init__(self, region: str):
+        logging.basicConfig(filename='parser.log', format='%(asctime)s %(levelname)s '
+                                                          '%(name)s %(message)s')
+        if not self.url:
+            raise InheritanceError('Child class should have an url attribute')
+        self.driver = None
+        self.update_driver()
+
+        self.region = region
+        self.set_region(region)
+
+        self.data = []
+        self.refresh_thread = None
+        self.update_data(init_call=True)
+
+    def update_driver(self):
+        if not getattr(self, 'driver', None):
+            options = ChromeOptions()
+            options.add_argument('--headless')
+            self.driver = Chrome(options=options)
+            self.driver.get(self.url)
+
+    def close_driver(self):
+        self.driver.close()
+
+    def set_refresh_process(self):
+        self.refresh_thread = Timer(self.interval, self.update_data)
+        self.refresh_thread.start()
+
+    def set_region(self, region: str):
+        self.region = region
+        self.select_region()
+
+    def select_region(self):
+        pass
+
+    def update_data(self, init_call=False):
+        pass
+
+    def get_data(self):
+        return self.data
---
+++ data/parsers/dixy.py
@@ -0,0 +1,96 @@
+import logging
+import time
+from datetime import timedelta
+from humanize import precisedelta
+
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver import Chrome, ChromeOptions
+
+from data.parsers.commonParser import CommonParser
+
+
+class DixyParser(CommonParser):
+    """Class that consists of products data of Dixy market in particular region and updates them"""
+
+    url: str = 'https://dixy.ru/catalog/'
+    interval: int = 3600
+
+    def __init__(self, region: str = 'Санкт-Петербург'):
+        super(DixyParser, self).__init__(region)
+
+    def update_driver(self):
+        if not getattr(self, 'driver', None):
+            options = ChromeOptions()
+            options.add_argument('--headless')
+            self.driver = Chrome(options=options)
+            self.driver.get(self.url)
+
+    def select_region(self):
+        self.driver.get(self.url)
+        region_select_menu = self.driver.find_element_by_class_name('icon-arrow-down')
+        if not region_select_menu:
+            return logging.error(message=f'Failed to get region select menu [IN {self}]')
+        region_select_menu.click()
+        region_buttons = self.driver.find_elements_by_xpath('//div[@class="simplebar-content"]/ul//li/a')
+        if not region_buttons:
+            return logging.error(message=f'Failed to get region buttons [IN {self}]')
+        try:
+            region_button = list(filter(lambda reg: reg.text.lower() == self.region.lower(),
+                                        region_buttons))[0]
+        except IndexError:
+            return logging.error(f'Failed to find {self.region} in region buttons [IN {self}]')
+        region_button.click()
+        return True
+
+    def scroll_to_bottom(self, tag: str, class_: str):
+        elem = True
+        been_toggled = False
+        while elem:
+            try:
+                elem = self.driver.find_element_by_xpath(f'//{tag}[@class="{class_}"]')
+            except NoSuchElementException:
+                elem = None
+            if not elem and not been_toggled:
+                return logging.warning(message='Failed to scroll to bottom', market=self)
+            elif elem:
+                been_toggled = True
+                try:
+                    elem.click()
+                except ElementClickInterceptedException:
+                    print('I AM STUCK')
+                    continue
+
+    def update_data(self, init_call=False, prev_data: list = None):
+        prev_data = prev_data if prev_data else []
+        if not init_call:
+            self.driver.refresh()
+        self.scroll_to_bottom('a', 'btn view-more')
+        self.data = []
+        products = self.driver.find_elements_by_class_name('dixyCatalogItem ')
+        for product in products:
+            try:
+                pic_block = product.find_element_by_class_name('dixyModal__pic'
+                                                               ).find_element_by_tag_name('img')
+                title = pic_block.get_attribute('alt').replace('\xa0', ' ')
+                if title in prev_data:
+                    continue
+                price = product.find_element_by_xpath('.//p[@itemprop="price"]').get_attribute('content')
+                self.data.append({'Название': title,
+                                  'Цена': price,
+                                  'Картинка': pic_block.get_attribute('src')})
+            except NoSuchElementException as e:
+                logging.warning(msg=f'{e.msg} [IN {self}]')
+
+        self.set_refresh_process()
+
+    def __repr__(self):
+        return f'Дикси. {self.region[0].upper() + self.region[1:]}'
+
+
+if __name__ == '__main__':
+    start_time = time.time()
+    dixy = DixyParser('Ленинградская область')
+    print('\n'.join([', '.join([f'{key}: {val}' for key, val in [item for item in product.items()]])
+                     for product in dixy.get_data()]))
+    seconds, microseconds = map(int, '{0:.2f}'.format(time.time() - start_time).split('.'))
+    print(f'Time passed: {precisedelta(timedelta(seconds=seconds, microseconds=microseconds))}')
